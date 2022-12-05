01c0d942eb8188aec76819ec852b5b2b6c6ecaa4
liaYaghobi <107156726+liaYaghobi@users.noreply.github.com>
2022-12-04 18:50:28 -0800

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,14 @@
+
+<?php
+
+$servername= "localhost";
+       $username = "root";
+       $pass = "";
+       $dbname = "lab4";
+ 
+       $conn = new mysqli($servername, $username, $pass, $dbname);
+       if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+          print("Failure!");
+       }
+      
